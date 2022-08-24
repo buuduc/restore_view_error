3854b5f6a537f6b185304e3ff05653adad22aa9d
ducnguyen <buuduc1999@gmail.com>
2022-08-24 12:07:03 +0700

    NEW: update again

--- getLostDb.js
+++ getLostDb.js
@@ -16,11 +16,18 @@ const createConnection = require('./helpers/createConnection')
             const db2 = await createConnection(uri2)
             const db1 = await createConnection(uri1)
             //* step 1: get data in db 2 to found the problem
-            const { rows: db2DataList } = await db2.query("SELECT * from analytics where updated_at between '2022-08-20' and '2022-08-23'")
-
-            await Promise.all(db2DataList.map(async (db2Data) => {
-                const { rows: [db1Data] } = await db1.query(`SELECT * from analytics where video_id='${db2Data.video_id}';`)
+            const listId = fs.readFileSync('./assets/video_view_cache.txt').toString().split("\r\n");
+            console.log('🚀 🚀 file: getLostDb.js 🚀 line 20 🚀 ; 🚀 listId', listId)
+            // const listId = ['9bf4cd77-f30a-4456-b1a5-0b676c89c364']
+            console.log('🚀 🚀 file: getLostDb.js 🚀 line 20 🚀 ; 🚀 listId', listId)
+            await Promise.all(listId.map(async (id) => {
+                console.log('🚀 🚀 file: getLostDb.js 🚀 line 22 🚀 awaitPromise.all 🚀 id', id)
+                if(!id) return
+                const { rows: [db1Data] } = await db1.query(`SELECT * from analytics where video_id='${id}';`)
+                console.log('🚀 🚀 file: getLostDb.js 🚀 line 25 🚀 awaitPromise.all 🚀 db1Data', db1Data)
+                const { rows: [db2Data] } = await db2.query(`SELECT * from analytics where video_id='${id}';`)
                 const returnData = { videoId: db2Data.video_id }
+                console.log('🚀 🚀 file: getLostDb.js 🚀 line 28 🚀 awaitPromise.all 🚀 db2Data', db2Data)
                 returnData.db2View = db2Data.video_view
                 if (db1Data) {
                     returnData.db1View = db1Data.video_view
@@ -32,13 +39,15 @@ const createConnection = require('./helpers/createConnection')
                 if (returnData.subtract === 0) return
 
                 if ((db1Data && db2Data.video_view > 30000 && db2Data.video_view / db1Data.video_view > 10)) {
+                    console.log('the data unexpected:', returnData)
                     viewErrorList.push(returnData)
                 } else {
+                    console.log('the good data',returnData)
                     viewIncreaseList.push(returnData)
                 }
             }))
             console.log(viewErrorList)
-            
+
             const fileHandler = fileHandle('restore_view.sql')
             fileHandler.deleteFile()
             fileHandler.writeUpdateView(viewIncreaseList)
@@ -51,7 +60,6 @@ const createConnection = require('./helpers/createConnection')
             await db2.end()
 
         } catch (error) {
-            console.log('file: compareLostVideo.js # line 20 # ; # error', error)
         }
         finally {
             // await db1.end()
--- restore_view_error.sql
+++
@@ -1,8 +0,0 @@
-UPDATE analytics SET video_view = (analytics.video_view + 3347 - 48253 + 100) WHERE video_id='c5224c22-824a-452e-8f4e-5423fbe17546'; 
-UPDATE analytics SET video_view = (analytics.video_view + 2718 - 43791 + 111) WHERE video_id='183bc9ca-f569-44e8-bd0b-73646b65480e'; 
-UPDATE analytics SET video_view = (analytics.video_view + 15891 - 1156335 + 177) WHERE video_id='b961b194-a6e1-4efb-ab62-7a96cdc212a0'; 
-UPDATE analytics SET video_view = (analytics.video_view + 2133 - 148934 + 160) WHERE video_id='2d47bdf4-1e26-4a8a-8e91-ede28943d50c'; 
-UPDATE analytics SET video_view = (analytics.video_view + 1270 - 67103 + 292) WHERE video_id='6cb4bc98-a433-43e4-b99f-854884c99341'; 
-UPDATE analytics SET video_view = (analytics.video_view + 177 - 178402 + 204) WHERE video_id='49dfe3df-4d2f-41c8-94a5-af0f36ede293'; 
-UPDATE analytics SET video_view = (analytics.video_view + 468 - 128575 + 235) WHERE video_id='11fc0deb-c156-489c-8b41-62652a2fc5ae'; 
-UPDATE analytics SET video_view = (analytics.video_view + 682 - 141861 + 158) WHERE video_id='cfaed4ec-1b40-4172-aef8-bbc95d4162be'; 
